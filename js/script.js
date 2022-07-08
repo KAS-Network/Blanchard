7e05d218e1d9104358da7e4334ba8b8a03623f69
KAS <kas.main.network@gmail.com>
2022-07-08 16:58:27 +0700

    Sub-menus were created

--- js/script.js
+++ js/script.js
@@ -1,41 +1,42 @@
-function inactivate(element) {
-  if(element.classList.contains("is-active")) {
-    element.classList.add("is-inactive");
-    document.querySelector(`.art-nav__title[data-path="${element.dataset.target}"]`).classList.remove("art-nav__title_active");
-    setTimeout(function() {
-      element.classList.remove("is-active");
-      element.classList.remove("is-inactive");
-    }, 200);
-  }
+function closeSubMenu(subMenu) {
+  subMenu.classList.add("sub-menu_inactive");
+  document.querySelector(`.art-nav__btn[data-path="${subMenu.dataset.target}"]`).classList.remove("art-nav__btn_active");
+  setTimeout(function() {
+      subMenu.classList.remove("sub-menu_active");
+      subMenu.classList.remove("sub-menu_inactive");
+  }, 300);
 }
 
-document.querySelectorAll(".art-nav__title").forEach(function(element) {
-  element.addEventListener("click", function(event) {
-    const btn = event.target;
-    const path = btn.dataset.path;
-    const subMenu = document.querySelector(`.sub-menu[data-target=${path}]`);
-    if(subMenu.classList.contains("is-active")) {
-      inactivate(subMenu);
-    }
-    else {
-      subMenu.classList.add("is-active");
-      btn.classList.add("art-nav__title_active")
-    }
+function closeAllSubMenus() {
+  document.querySelectorAll(".sub-menu.sub-menu_active").forEach(function(subMenu) {
+      closeSubMenu(subMenu);
+  });
+  document.body.removeEventListener("click", closeAllSubMenus);
+}
+
+document.querySelectorAll(".art-nav__btn").forEach(function(btn) {
+  btn.addEventListener("click", function(click) {
+      click.stopPropagation();
+      const path = btn.dataset.path;
+      const targetSubMenu = document.querySelector(`.sub-menu[data-target="${path}"]`);
+      if(targetSubMenu.classList.contains("sub-menu_active")) {
+          closeSubMenu(targetSubMenu);
+          btn.classList.remove("art-nav__btn_active");
+          document.body.removeEventListener("click", closeAllSubMenus);
+      }
+      else {
+          document.querySelectorAll(".sub-menu.sub-menu_active").forEach(function(subMenu) {
+              closeSubMenu(subMenu);
+          });
+          targetSubMenu.classList.add("sub-menu_active");
+          btn.classList.add("art-nav__btn_active");
+          document.body.addEventListener("click", closeAllSubMenus);
+      }
   });
 });
 
-document.body.addEventListener("click", function(event) {
-  const menus = document.querySelectorAll(".sub-menu");
-  if(event.target.classList.contains("art-nav__title")) {
-    menus.forEach(function(element) {
-      if(element.dataset.target != event.target.dataset.path) {
-        inactivate(element);
-      }
-    });
-  }
-  else {
-    menus.forEach(function(element) {
-      inactivate(element);
-    });
-  }
+document.querySelectorAll(".sub-menu__list").forEach(function(subMenuList) {
+  new SimpleBar(subMenuList, {
+    autoHide: false
+  })
 });
