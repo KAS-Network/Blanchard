c61254f21ebf7db9b0a3935fe502831d347ccdaf
KAS <kas.main.network@gmail.com>
2022-07-15 18:50:25 +0700

    section was completed

--- js/script.js
+++ js/script.js
@@ -85,15 +85,20 @@ function ModalWindowExit(event) {
 }
 
 function closeModalWindow(click) {
+  const picture = document.querySelector(".modal-window__picture_selected")
+  const target = picture.dataset.target;
+  const slide = document.querySelector(`.gallery-swiper__slide[data-path="${target}"]`);
   document.body.classList.remove("body_inactive");
   document.querySelector(".header").removeAttribute("inert");
   document.querySelector(".main").removeAttribute("inert")
   document.querySelector(".footer").removeAttribute("inert");
-  document.querySelector(".modal-window__picture_selected").classList.remove("modal-window__picture_selected");
+  picture.classList.remove("modal-window__picture_selected");
   document.querySelector(".modal-window__content_selected").classList.remove("modal-window__content_selected");
   document.querySelector(".modal-window").classList.remove("modal-window_active");
   document.querySelector(".modal-window__close-btn").removeEventListener("click", closeModalWindow);
+  click.target.removeEventListener("click", closeModalWindow);
   document.removeEventListener("keydown", ModalWindowExit);
+  slide.focus();
 }
 
 const gallerySlides = document.querySelectorAll(".gallery-swiper__slide");
@@ -118,8 +123,10 @@ gallerySlides.forEach(function(slide) {
 function closeAccordionBlock(trigger, block) {
   trigger.classList.remove("accordion__trigger_active");
   block.style.maxHeight = "0";
+  const accordionItem = trigger.parentElement;
   setTimeout(function() {
     block.classList.remove("expansion-block_visible");
+    accordionItem.removeAttribute("data-active");
   }, 500);
 }
 
@@ -139,8 +146,10 @@ triggerList.forEach(function(trigger) {
       if(activeTrigger) {
         closeAccordionBlock(activeTrigger, activeBlock);
       }
+      const accordionItem = trigger.parentElement;
       trigger.classList.add("accordion__trigger_active");
       targetBlock.classList.add("expansion-block_visible");
+      accordionItem.setAttribute("data-active", true);
       if(targetList) {
         targetBlock.style.maxHeight = targetList.offsetHeight + "px";
         targetCork.classList.add("cork_disabled");
@@ -156,6 +165,27 @@ triggerList.forEach(function(trigger) {
   trigger.addEventListener("keydown", function(event) {
     if(event.keyCode === 13) {
       trigger.click();
+    }
+  });
+});
+
+const artistBtns = document.querySelectorAll(".expansion-block__btn");
+artistBtns.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    const selectedPicture = document.querySelector(".about-artist__picture_selected");
+    const selectedInfo = document.querySelector(".about-artist__info_selected");
+    selectedPicture.classList.remove("about-artist__picture_selected");
+    selectedInfo.classList.remove("about-artist__info_selected");
+    const name = btn.dataset.name;
+    const targetPicture = document.querySelector(`.about-artist__picture[data-target="${name}"]`);
+    const targetInfo = document.querySelector(`.about-artist__info[data-target="${name}"]`);
+    if(targetPicture) {
+      targetPicture.classList.add("about-artist__picture_selected");
+      targetInfo.classList.add("about-artist__info_selected");
+    }
+    else {
+      document.querySelector(".about-artist__picture:not([data-target])").classList.add("about-artist__picture_selected");
+      document.querySelector(".about-artist__info:not([data-target])").classList.add("about-artist__info_selected");
     }
   });
 });
